107ff89665f9e29d887a1a3bd78a63e49665a22a
Lena <summyalena@gmail.com>
2022-08-12 16:22:57 +0100

    finished todolist-structure

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# Todo-List-App
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# Todo-App
+This is a todo-app
+
+
+## Built With
+
+- Major languages: HTML, CSS, JS
+
+## Getting Started
+To get a local copy and running follow these simple example steps.
+Prerequisites
+Setup
+Install
+Usage
+Run tests
+Deployment
+
+## Authors
+
+👤 **Author1**
+
+- GitHub: [@summyalena](https://github.com/summyalena)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Hat tip to all the Microverse 
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
+
+
--- src/index.js
+++ src/index.js
@@ -1,41 +1,5 @@
 import './style.css';
 
-const tasks = [
-  {
-    description: 'Cook for my family',
-    completed: false,
-    index: 0,
-  },
+import * as create from './create';
 
-  {
-    description: 'Finish the todo list',
-    completed: false,
-    index: 1,
-  },
-
-  {
-    description: 'Play with little kids',
-    completed: false,
-    index: 2,
-  },
-];
-
-const todo = document.querySelector('.todo-list');
-
-tasks.forEach((task, index) => {
-  if (task.index === index) {
-    todo.innerHTML += `
-        <div class="Lists">
-         <div class="list">
-         <div class="list-one">
-         <input type="checkbox" bool=${task.completed}/>
-         <p>${task.description}</p>
-         </div>
-         <div class="dots">
-         <i class="fa-solid fa-ellipsis-vertical"></i>
-         </div>
-         </div>
-        </div>
-        `;
-  }
-});
+create();
